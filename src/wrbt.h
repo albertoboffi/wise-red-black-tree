c8b78a1ca329068a13b7172045da78aae6db037e
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2021-05-15 19:20:29 +0200

    feat: create node structure

---
+++ src/wrbt.h
@@ -0,0 +1,13 @@
+//node strucutre
+typedef enum{red='0', black='0'} nodeColor;
+
+typedef struct node{
+  int key;
+  nodeColor color;
+  struct node *left;
+  struct node *right;
+  struct node *p;
+  int leftSize;
+
+  char *data;
+}node_t;
