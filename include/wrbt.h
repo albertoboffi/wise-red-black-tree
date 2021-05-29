4b25ae15158e312cbce95f8bad3d4c1e7118e24e
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2021-05-29 17:38:56 +0200

    feat: add functions to header file

--- include/wrbt.h
+++ include/wrbt.h
@@ -1,4 +1,3 @@
-//node strucutre
 typedef enum{red='r', black='b'} nodeColor;
 
 typedef struct node{
@@ -12,4 +11,10 @@ typedef struct node{
   char *data;
 }node_t;
 
-//operations
+extern node_t *TRoot, *TLeaves;
+node_t *kthSmallest(node_t *R, int k);
+node_t *inOrderSuccessor(node_t *X);
+void append(char *data);
+node_t *insertFirst(int key, char *data, int m);
+node_t *insertSubsq(node_t *prev, int key, char *data);
+void delete(int k, int m);
